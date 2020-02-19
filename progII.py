2408b9fc8676cc8472120e6a1c3fdff8ad344371
Aluno <gabrileh.gta@gmail.com>
2020-02-19 09:21:31 -0300

    Lógica de Programação

--- progII.py
+++ progII.py
@@ -2,3 +2,12 @@ print("Programação II")
 print("Hello World")
 print("OI")
 print("Aula de Programação II")
+
+# entrada
+nome = input("Digite seu nome: ")
+
+# processamento
+a = 0
+while a < 10:
+    print (nome) 
+    a += 1 
